37a18c8da02876966d1267c1e8ec5d19decdd69d
radil <adil.r@northeastern.edu>
2022-12-10 16:39:10 -0500

    - Big image fix

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -2,8 +2,17 @@ import Tuits from "../tuits";
 import React from "react";
 
 const MyLikes = () => {
+    const [likedTuits, setLikedTuis] = useState([]);
+    const findTuitsILike = () =>
+        service.findAllTuitsLikedByUser("me")
+            .then((tuits) => setLikedTuis(tuits));
+
+    useEffect(findTuitsILike, []);
+
+
     return (
-        <h1>My likes</h1>)
+        <h1>My likes</h1>
+    )
 
 }
 export default MyLikes
--- src/components/tuits/tuit-stats.js
+++ src/components/tuits/tuit-stats.js
@@ -1,28 +1,61 @@
 import React from "react";
 
-export default class TuitStats extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+const TuitStats = ({tuit, likeTuit}) => {
+    // some tuits still use old schema, can't really
+    //interact with them but can make sure they have a
+    //default stats info
+    if (tuit.stats == null) {
+        tuit.stats = {
+            replies:  0,
+            retuits:  0,
+            likes: 0
+        }
+    }
     return (
-      <div className="row mt-2">
-        <div className="col">
-          <i className="far fa-message me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.replies}
+        <div className="row mt-2">
+            <div className="col">
+                <span onClick={() => likeTuit(tuit)}>
+                    { tuit.stats.likes > 0  &&
+                        <i className="fas fa-heart" style={{color: 'red'}}></i>
+                    }
+                    {
+                        tuit.stats.likes <= 0 &&
+                        <i className="far fa-heart"></i>
+                    }
+                    {
+                        tuit.stats && tuit.stats.likes
+                    }
+                </span>
+            </div>
         </div>
-        <div className="col">
-          <i className="far fa-retweet me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.retuits}
-        </div>
-        <div className="col">
-          <i className="far fa-heart me-1"></i>
-          {this.props.tuit.stats && this.props.tuit.stats.likes}
-        </div>
-        <div className="col">
-          <i className="far fa-inbox-out"></i>
-        </div>
-      </div>
-    );
-  }
+    )
 }
+
+export default TuitStats
+
+// export default class TuitStats extends React.Component {
+//   constructor(props) {
+//     super(props);
+//   }
+//   render() {
+//     return (
+//       <div className="row mt-2">
+//         <div className="col">
+//           <i className="far fa-message me-1"></i>
+//           {this.props.tuit.stats && this.props.tuit.stats.replies}
+//         </div>
+//         <div className="col">
+//           <i className="far fa-retweet me-1"></i>
+//           {this.props.tuit.stats && this.props.tuit.stats.retuits}
+//         </div>
+//         <div className="col">
+//           <i className="far fa-heart me-1"></i>
+//           {this.props.tuit.stats && this.props.tuit.stats.likes}
+//         </div>
+//         <div className="col">
+//           <i className="far fa-inbox-out"></i>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
