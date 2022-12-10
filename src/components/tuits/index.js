9f6aa8fed4d03f1ea3d9194a8ebb534860b97f2c
radil <adil.r@northeastern.edu>
2022-12-10 15:10:08 -0500

    - Big image?

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -1,21 +1,61 @@
 import React from "react";
 import './tuits.css';
 import Tuit from "./tuit";
+import {toggleLikes} from "../../services/likes-service";
+import {profile} from "../../services/auth-service";
 
-function Tuits({tuits = [], deleteTuit}) {
-    return (
-    <div>
-      <ul className="ttr-tuits list-group">
-        {
-          tuits.map && tuits.map(tuit => {
-            return (
-              <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
-            );
-          })
+const Tuits = ({tuits = [], deleteTuit,
+                   refreshTuits}) => {
+
+    // const likeTuit = (tuit) =>
+    //     likesService
+    //         .userTogglesTuitLikes("me", tuit._id)
+    //         .then(refreshTuits)
+    //         .catch(e => alert(e))
+
+    const likeTuit = async (tuit) => {
+        // get username
+        let userInfo = await profile()
+        const currentUserId = userInfo.userId
+        // in my implmentation tuit id is in the tuitID property
+        try{
+            await toggleLikes(currentUserId,tuit.tuitID)
+            refreshTuits()
         }
-      </ul>
-    </div>
-  );
+        catch (e) {
+            alert(e)
+        }
+    }
+    return (
+        <div>
+            <ul>
+                {
+                    tuits.map(tuit =>
+                        <Tuit key={tuit.tuitID}
+                              deleteTuit={deleteTuit}
+                              likeTuit={likeTuit}
+                              tuit={tuit}/>)
+                }
+            </ul>
+        </div>
+    );
 }
+
+
+// function Tuits({tuits = [], deleteTuit}) {
+//     return (
+//     <div>
+//       <ul className="ttr-tuits list-group">
+//         {
+//           tuits.map && tuits.map(tuit => {
+//             return (
+//               <Tuit key={tuit._id} deleteTuit={deleteTuit} tuit={tuit}/>
+//             );
+//           })
+//         }
+//       </ul>
+//     </div>
+//   );
+// }
 
 export default Tuits;
