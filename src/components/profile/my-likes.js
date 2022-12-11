df6b3e3e94d6fa1a19d642c615b4891d47a6806c
radil <adil.r@northeastern.edu>
2022-12-10 21:34:06 -0500

    - Fixed bug

--- src/components/profile/my-likes.js
+++ src/components/profile/my-likes.js
@@ -2,7 +2,7 @@ import Tuits from "../tuits";
 import React, {useEffect, useState} from "react";
 import {profile} from "../../services/auth-service";
 import * as likeService from "../../services/likes-service";
-
+//fixed bug
 const MyLikes = () => {
     const [likedTuits, setLikedTuits] = useState([]);
 
